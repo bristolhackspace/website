7c12c91682febf9aa885a249b459d3a98333be94
Sam <633856+SamP20@users.noreply.github.com>
2024-11-09 16:24:08 +0000

    Basic contact form

--- hackspace_website/__init__.py
+++ hackspace_website/__init__.py
@@ -3,6 +3,11 @@ import logging
 import tomllib
 
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, EmailField, IntegerField, StringField, ValidationError
+from wtforms.validators import InputRequired, Length, DataRequired
+
+from hackspace_website.models import db, Message
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -26,8 +31,35 @@ def create_app(test_config=None):
     def visit():
         return render_template("pages/visit.html")
 
-    @app.route("/contact")
+
+    class ContactForm(FlaskForm):
+        name = StringField("Name", validators=[
+            InputRequired(),
+            Length(max=Message.name.type.length)
+        ])
+        email = EmailField("Email", validators=[
+            InputRequired(),
+            Length(max=Message.email.type.length)
+        ])
+        subject = StringField("Subject", validators=[
+            InputRequired(),
+            Length(max=Message.subject.type.length)
+        ])
+        message = StringField("Message", validators=[
+            InputRequired(),
+            Length(max=Message.message.type.length)
+        ])
+        privacy = BooleanField("Privacy agreement", validators=[DataRequired()])
+
+
+
+    @app.route("/contact", methods=["GET", "POST"])
     def contact():
-        return render_template("pages/contact.html")
+        form = ContactForm()
+
+        if form.validate_on_submit():
+            return render_template("pages/contact_success.html")
+
+        return render_template("pages/contact.html", form=form)
 
     return app
