d7084d34a036f5c6c97bcf7b668e1b058734bcba
SamP20 <633856+SamP20@users.noreply.github.com>
2024-11-10 11:27:06 +0000

    Contact form styling

--- hackspace_website/__init__.py
+++ hackspace_website/__init__.py
@@ -4,16 +4,23 @@ import tomllib
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, EmailField, IntegerField, StringField, ValidationError
+from markupsafe import Markup
+from wtforms import (
+    BooleanField,
+    EmailField,
+    TextAreaField,
+    StringField,
+    ValidationError,
+)
 from wtforms.validators import InputRequired, Length, DataRequired
 
 from hackspace_website.models import db, Message
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/website",
-
     )
     if test_config is None:
         app.config.from_file("config.toml", load=tomllib.load, text=False)
@@ -21,6 +28,7 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     from . import models
+
     models.init_app(app)
 
     @app.route("/")
@@ -31,27 +39,35 @@ def create_app(test_config=None):
     def visit():
         return render_template("pages/visit.html")
 
-
     class ContactForm(FlaskForm):
-        name = StringField("Name", validators=[
-            InputRequired(),
-            Length(max=Message.name.type.length)
-        ])
-        email = EmailField("Email", validators=[
-            InputRequired(),
-            Length(max=Message.email.type.length)
-        ])
-        subject = StringField("Subject", validators=[
-            InputRequired(),
-            Length(max=Message.subject.type.length)
-        ])
-        message = StringField("Message", validators=[
-            InputRequired(),
-            Length(max=Message.message.type.length)
-        ])
-        privacy = BooleanField("Privacy agreement", validators=[DataRequired()])
-
-
+        name = StringField(
+            "Name",
+            name="iy5ku",
+            validators=[InputRequired(), Length(max=Message.name.type.length)],
+        )
+        email = EmailField(
+            "Email",
+            name="oeu2b",
+            validators=[InputRequired(), Length(max=Message.email.type.length)],
+        )
+        subject = StringField(
+            "Subject",
+            name="tx5jm",
+            validators=[InputRequired(), Length(max=Message.subject.type.length)],
+        )
+        message = TextAreaField(
+            "Message",
+            name="fc2ah",
+            validators=[InputRequired(), Length(max=Message.message.type.length)],
+        )
+        privacy = BooleanField(
+            Markup(
+                'I have read and agree to the terms of the <a href="https://wiki.bristolhackspace.org/policies/privacy">Privacy Policy</a>'
+            ),
+            name="um90x",
+            validators=[DataRequired()],
+        )
+        honeypot = BooleanField("Fax only", name="contact_me_by_fax_only")
 
     @app.route("/contact", methods=["GET", "POST"])
     def contact():
