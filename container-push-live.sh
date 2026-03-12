980a1b4e931e83bb62aa2269e8295120d91495c9
Aaron Shrimpton <aaronds@gmail.com>
2026-03-12 22:50:15 +0000

    Push to live date tag

--- container-push-live.sh
+++ container-push-live.sh
@@ -2,7 +2,11 @@
 
 CONTAINER_CMD=$(which podman || which docker)
 
+DATE_TAG=$(date +%Y%m%d)
+
 $CONTAINER_CMD tag registry.bristolhackspace.org/website:staging registry.bristolhackspace.org/website:live
+$CONTAINER_CMD tag registry.bristolhackspace.org/website:staging registry.bristolhackspace.org/website:$DATE_TAG
 
 $CONTAINER_CMD push registry.bristolhackspace.org/website:live
+$CONTAINER_CMD push registry.bristolhackspace.org/website:$DATE_TAG
 
